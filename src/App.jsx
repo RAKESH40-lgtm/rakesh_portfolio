9b2a9af16dc5d47bdb8cebe6b0df47a96ff46fc5
RAKESH40-lgtm <ram19042k@protonmail.com>
2026-02-07 23:10:08 +0530

    updated filepath

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import Header from './Components/Header';
 import Hero from './Components/Hero';
 import About from './Components/About';
 import Experience from './Components/Experience';
